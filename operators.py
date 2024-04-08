ca60781a60367877e0312c1495d6e433fe26d31c
CodeNestAcademy <166374804+CodeNestAcademy@users.noreply.github.com>
2024-04-08 14:03:29 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,37 @@
+#Assignment operators
+x = 2
+print(x)
+
+x = x+2
+print(x)
+
+x += 2
+print(x)
+
+x -= 2
+print(x)
+
+x %= 2
+print(x)
+
+x *= 2
+print(x)
+
+#Comparison operator
+n1 = 5
+n2 = 3
+print(n1 == n2)
+print(n1 != n2) #n1 is not equal to n2
+print(n1 >= n2) # n1 > n2 or n1 = n2
+print(n1 <= n2) # n1 < n2 or n1 = n2
+print(n1  < n2) 
+print(n1 > n2)
+
+
+#Logical operators (and, or, not)
+n1 = 6
+n2 = 7
+print(n1>7 and n2<8)
+print(n1 > 7 or n2<8)
+print(not n1 > 7) #not gives inverse of the right answer. If it's true then false and vice versa.
+
