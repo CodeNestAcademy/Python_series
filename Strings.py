3dad6a4dbbe1b55b3fb21b15c37fb63aa9f663a8
CodeNestAcademy <166374804+CodeNestAcademy@users.noreply.github.com>
2024-08-14 12:17:05 +0600

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,21 @@
+
+# Escape characters
+names = "xyz \n abc"
+print(names)
+
+# Multilines
+sentence = """I love Programming. 
+I want to learn more about python.
+I love to make projects in python."""
+print(sentence)
+
+# string slicing
+string = "string"
+print(string[0:6:3])
+# [start:end:step]
+# step = n, n-1 characters will be skipped till the end
+
+# Negative indexing
+greet = "Hello, user"
+# print(greet[-4:])
+print(greet[::-1])
